90d3cc2b2b647725a771ec05232a7614ef67e17f
João Seixas <myownxdeath@users.noreply.github.com>
2017-03-12 15:29:12 +0000

    Corrected MyPrism.js - updated tag TP3_1.3

--- WebCGF.tp3/tp3/MyPrism.js
+++ WebCGF.tp3/tp3/MyPrism.js
@@ -2,48 +2,44 @@
  * MyPrism
  * @constructor
  */
- function MyPrism(scene, slices, stacks) {
- 	CGFobject.call(this,scene);
-	
-	this.slices = slices;
-	this.stacks = stacks;
+function MyPrism(scene, slices, stacks) {
+    CGFobject.call(this, scene);
 
- 	this.initBuffers();
- };
+    this.slices = slices;
+    this.stacks = stacks;
 
- MyPrism.prototype = Object.create(CGFobject.prototype);
- MyPrism.prototype.constructor = MyPrism;
+    this.initBuffers();
+}
+;MyPrism.prototype = Object.create(CGFobject.prototype);
+MyPrism.prototype.constructor = MyPrism;
 
- MyPrism.prototype.initBuffers = function() {
- 
-	this.vertices = [];
-	this.indices = [];
-	this.normals = [];
- 	var ang = Math.PI*2/this.slices;
-	
+MyPrism.prototype.initBuffers = function() {
 
-	for(j = 0; j < this.stacks ; j++){
- 		for(i = 0; i < this.slices*2 ; i++){
-		
- 			this.vertices.push(Math.cos(i*ang), Math.sin(i*ang), j);
- 			this.vertices.push(Math.cos(i*ang), Math.sin(i*ang), j+1);
- 			this.vertices.push(Math.cos((i+1)*ang), Math.sin((i+1)*ang), j);
- 			this.vertices.push(Math.cos((i+1)*ang), Math.sin((i+1)*ang), j+1);
+    this.vertices = [];
+    this.indices = [];
+    this.normals = [];
+    var ang = Math.PI * 2 / this.slices;
 
+    for (j = 0; j < this.stacks; j++) {
+        for (i = 0; i < this.slices; i++) {
 
-			this.normals.push(Math.cos((i+0.5)*ang), Math.sin((i+0.5)*ang), 0);
- 			this.normals.push(Math.cos((i+0.5)*ang), Math.sin((i+0.5)*ang), 0);
- 			this.normals.push(Math.cos((i+0.5)*ang), Math.sin((i+0.5)*ang), 0);
- 			this.normals.push(Math.cos((i+0.5)*ang), Math.sin((i+0.5)*ang), 0);
+            this.vertices.push(Math.cos(i * ang), Math.sin(i * ang), j);
+            this.vertices.push(Math.cos(i * ang), Math.sin(i * ang), j + 1);
+            this.vertices.push(Math.cos((i + 1) * ang), Math.sin((i + 1) * ang), j);
+            this.vertices.push(Math.cos((i + 1) * ang), Math.sin((i + 1) * ang), j + 1);
 
+            this.normals.push(Math.cos((i + 0.5) * ang), Math.sin((i + 0.5) * ang), 0);
+            this.normals.push(Math.cos((i + 0.5) * ang), Math.sin((i + 0.5) * ang), 0);
+            this.normals.push(Math.cos((i + 0.5) * ang), Math.sin((i + 0.5) * ang), 0);
+            this.normals.push(Math.cos((i + 0.5) * ang), Math.sin((i + 0.5) * ang), 0);
 
- 			this.indices.push(1+(2*i)+(4*this.slices*j), 0+(2*i)+(4*this.slices*j), 2+(2*i)+(4*this.slices*j));
- 			this.indices.push(2+(2*i)+(4*this.slices*j), 3+(2*i)+(4*this.slices*j), 1+(2*i)+(4*this.slices*j));
+            this.indices.push(1 + (4 * i) + (4 * this.slices * j), 0 + (4 * i) + (4 * this.slices * j), 2 + (4 * i) + (4 * this.slices * j));
+            this.indices.push(2 + (4 * i) + (4 * this.slices * j), 3 + (4 * i) + (4 * this.slices * j), 1 + (4 * i) + (4 * this.slices * j));
 
+        }
+    }
 
- 		}
-	}
-	
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
- };
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+}
+;
